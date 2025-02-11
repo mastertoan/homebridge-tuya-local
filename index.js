a3c7b839206eb9238e2b266efeda5f1029579c66
mastertoan <73209064+mastertoan@users.noreply.github.com>
2025-02-11 12:00:00 +0700

    update

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const SimpleFanLightAccessory = require('./lib/SimpleFanLightAccessory');
 const SwitchAccessory = require('./lib/SwitchAccessory');
 const ValveAccessory = require('./lib/ValveAccessory');
 const OilDiffuserAccessory = require('./lib/OilDiffuserAccessory');
+const TankALevelAccessory = require('./lib/TankALevelAccessory');
 
 const PLUGIN_NAME = 'homebridge-tuya-local';
 const PLATFORM_NAME = 'TuyaLan';
@@ -47,7 +48,8 @@ const CLASS_DEF = {
     fan: SimpleFanAccessory,
     fanlight: SimpleFanLightAccessory,
     watervalve: ValveAccessory,
-    oildiffuser: OilDiffuserAccessory
+    oildiffuser: OilDiffuserAccessory,
+    tankalevel: TankALevelAccessory
 };
 
 let Characteristic, PlatformAccessory, Service, Categories, AdaptiveLightingController, UUID;
